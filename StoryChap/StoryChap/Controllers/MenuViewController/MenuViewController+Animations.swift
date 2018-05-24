f10c6d0a50c995aab41a72e8566ffdff0feb246e
Rhullian Damião <rhullian4545@gmail.com>
2018-05-24 19:13:49 -0300

    Banner, app icon and description screen done

---
+++ StoryChap/StoryChap/Controllers/MenuViewController/MenuViewController+Animations.swift
@@ -0,0 +1,27 @@
+//
+//  MenuViewController+Animations.swift
+//  StoryChap
+//
+//  Created by Rhullian Damião on 22/05/2018.
+//  Copyright © 2018 Rodrigo Hilkner. All rights reserved.
+//
+
+import UIKit
+
+extension MenuViewController {
+    /// Function resposible for doing the expantion of menu collection view
+    func animateExpandMenuCollectionVew() {
+        self.menuCollectionHeightConstraint.constant += 220
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    /// Function resposible for doing the expantion of menu collection view
+    func animateReduceMenuCollectionVew() {
+        self.menuCollectionHeightConstraint.constant -= 220
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+}
